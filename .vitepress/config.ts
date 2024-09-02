73e4e2c8122b18c8af269d14c686ea0c976e2209
Riley Ho <riley@rileychh.dev>
2024-09-02 23:23:51 +0800

    chore: add explaination about importing with relative paths

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vitepress'
 import Components from 'unplugin-vue-components/vite'
 import Icons from 'unplugin-icons/vite'
 import IconsResolver from 'unplugin-icons/resolver'
+
+// Subpath imports (e.g. '#data') for TypeScript files are not supported
+// See https://github.com/vuejs/vitepress/issues/4173
 import { conference } from '../data/conference'
 
 // https://vitepress.dev/reference/site-config
