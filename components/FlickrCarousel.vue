4dd68f85600706389bb65d19d9f1a758b2a22c86
Riley Ho <riley@rileychh.dev>
2024-12-08 17:32:19 +0800

    feat: import carousel images dynamically

--- components/FlickrCarousel.vue
+++ components/FlickrCarousel.vue
@@ -1,20 +1,12 @@
 <script setup>
 import { register } from 'swiper/element/bundle'
 
-import image1 from '#assets/images/image1.webp'
-import image2 from '#assets/images/image2.webp'
-import image3 from '#assets/images/image3.webp'
-import image4 from '#assets/images/image4.webp'
-import image5 from '#assets/images/image5.webp'
-import image6 from '#assets/images/image6.webp'
-import image7 from '#assets/images/image7.webp'
-import image8 from '#assets/images/image8.webp'
-import image9 from '#assets/images/image9.webp'
-import image10 from '#assets/images/image10.webp'
-
 register()
 
-const images = [image1, image2, image3, image4, image5, image6, image7, image8, image9, image10]
+const images = import.meta.glob('#assets/images/professional-portraits/*.webp', {
+  eager: true,
+  import: 'default',
+})
 </script>
 
 <template>
